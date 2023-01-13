7e80643d4dc9749a3da7ede7e371a96bd6f06e95
Aayush Gupta <guptaaayush432@gmail.com>
2023-01-13 13:17:01 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Token = await hre.ethers.getContractFactory("OurToken");
-  const token = await Token.deploy(50); // 50 initial supply
-  await token.deployed();
-  console.log("token deployed to:", token.address);
+  const Contract = await hre.ethers.getContractFactory("LunchMoney");
+  const contract = await Contract.deploy();
+  await contract.deployed();
+  console.log("Contract deployed to:", contract.address);
 
   console.log("Sleeping.....");
   // Wait for etherscan to notice that the contract has been deployed
@@ -12,9 +12,9 @@ async function main() {
 
   // Verify the contract after deploying
   await hre.run("verify:verify", {
-    address: token.address,
-    constructorArguments: [50],
-    contract: "contracts/OurToken.sol:OurToken",
+    address: contract.address,
+    constructorArguments: [],
+    contract: "contracts/LunchMoney.sol:LunchMoney",
   });
 }
 
