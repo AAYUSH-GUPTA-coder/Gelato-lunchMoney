2f05f0698c23e92e245c682ce4acc7f4a9c6561c
Aayush Gupta <guptaaayush432@gmail.com>
2023-01-13 14:20:47 +0530

    Update LunchMoney.sol

--- contracts/LunchMoney.sol
+++ contracts/LunchMoney.sol
@@ -1,23 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+error ONLYOWNER_CAN_CALL_THIS_FUNCTION();
+error ADDRESS_CANT_BE_ZERO();
+error AMOUNT_CANT_BE_ZERO();
+error TIME_NOT_PASSED();
+error FAILED_TO_SEND_ETHER();
+
 contract LunchMoney{
     address private receiver;
     uint256 private lunchMoneyAmount;
+    address private owner;
+    uint private threshold;
+    uint private lastCall;
 
-    function setReceiverAddress(address _receiver) public {
-        require(_receiver != address(0x0), "address cannot be the zero address");
+    constructor() {
+        owner = msg.sender;
+        lastCall = block.timestamp;
+    }
+
+    modifier onlyOwner() {
+        if(msg.sender != owner){
+            revert ONLYOWNER_CAN_CALL_THIS_FUNCTION();
+        }
+        _;
+    }
+
+    function setReceiverAddress(address _receiver) public onlyOwner {
+        if(_receiver == address(0x0)){
+            revert ADDRESS_CANT_BE_ZERO();
+        }
         receiver = _receiver;
     }
 
-    function setLunchMoneyAmount(uint256 _amount) public {
-        require(_amount > 0, "can't be zero");
+    function setLunchMoneyAmount(uint256 _amount) public onlyOwner {
+        if(_amount == 0){
+            revert AMOUNT_CANT_BE_ZERO();
+        }
         lunchMoneyAmount = _amount;
     }
 
+    // production
+    // function setThreshold(uint8 _hours) public onlyOwner {
+    //     threshold = _hours * 60 * 60;
+    // }
+
+    // testing 
+    function setThreshold(uint8 _min) public onlyOwner {
+        threshold = _min * 60 ;
+    }
+
     function transferLunchMoney() public { 
+        if(block.timestamp < lastCall + threshold){
+            revert TIME_NOT_PASSED();
+        }
         (bool sent, ) =  receiver.call{value: lunchMoneyAmount}("");
-        require(sent, "Failed to send Ether");
+        if(!sent){
+            revert FAILED_TO_SEND_ETHER();
+        }
+        lastCall = block.timestamp;
+    }
+
+    function withdraw() public onlyOwner {
+        (bool sent, ) =  owner.call{value: address(this).balance}("");
+        if(!sent){
+            revert FAILED_TO_SEND_ETHER();
+        }
     }
 
     function getBalance() public view returns(uint){
@@ -30,6 +78,18 @@ contract LunchMoney{
 
     function getLunchAmount() public view returns(uint){
         return lunchMoneyAmount;
+    }
+
+    function getLastCall() public view returns(uint){
+        return lastCall;
+    }
+
+    function getOwner() public view returns(address){
+        return owner;
+    }
+
+    function getTimeRemaining() public view returns(uint){
+        return lastCall + threshold - block.timestamp;
     }
 
     // receive function is used to receive Ether when msg.data is empty
