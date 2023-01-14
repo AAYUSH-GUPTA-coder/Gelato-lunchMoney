3dc2dc3914885bec737ab60d0ff2220a7993619f
Aayush Gupta <guptaaayush432@gmail.com>
2023-01-14 14:48:36 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.7",
   networks: {
     mumbai: {
       url: QUICKNODE_MUMBAI_HTTP_URL,
